2d57bc1d8b9e5ec95b502d18841f281f461c5714
Matthijs Lubbers <m.lubbers@gmail.com>
2020-02-09 15:51:06 +0100

    Add remove.sh to delete the images and volumes

--- remove.sh
+++ remove.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 
-#remove-all: down remove-image-all remove-volume-all ## Remove all project conatiners, images and volumes
-#
-#remove-image-all: remove-image-php remove-image-nginx ## Remove all project images
-#
-#remove-image-php: ## Remove project php image
-#	docker image rm --force $(COMPOSE_PROJECT_NAME)-php
-#
-#remove-image-nginx: ## Remove project nginx image
-#	docker image rm --force $(COMPOSE_PROJECT_NAME)-nginx
-#
-#remove-volume-all: remove-volume-mysql remove-volume-redis ## Remove all project volumes
-#
-#remove-volume-mysql: ## Remove project mysql volume
-#	docker volume rm --force $(COMPOSE_PROJECT_NAME)-mysql
-#
-#remove-volume-redis: ## Remove project redis volume
-#	docker volume rm --force $(COMPOSE_PROJECT_NAME)-redis
+# Get container-prefix from the docker-compose .env file
+COMPOSE_PROJECT_NAME="$(grep COMPOSE_PROJECT_NAME .env | xargs)"
+IFS='=' read -ra COMPOSE_PROJECT_NAME <<< "$COMPOSE_PROJECT_NAME"
+CONTAINER_PREFIX="${COMPOSE_PROJECT_NAME[1]}"
+
+# Set output colors and spacing
+DEFAULT="\033[0m"
+MAIN="\033[32m"
+SUB="\033[33m"
+
+read -p "Remove all Docker images for this project? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  printf "${MAIN}Removing: ${SUB}%s${DEFAULT}..\n" "${CONTAINER_PREFIX}"-mysql
+  docker image rm --force "${CONTAINER_PREFIX}"-mysql
+
+  printf "${MAIN}Removing: ${SUB}%s${DEFAULT}..\n" "${CONTAINER_PREFIX}"-redis
+  docker image rm --force "${CONTAINER_PREFIX}"-redis
+
+  printf "${MAIN}Removing: ${SUB}%s${DEFAULT}..\n" "${CONTAINER_PREFIX}"-php
+  docker image rm --force "${CONTAINER_PREFIX}"-php
+
+  printf "${MAIN}Removing: ${SUB}%s${DEFAULT}..\n" "${CONTAINER_PREFIX}"-nginx
+  docker image rm --force "${CONTAINER_PREFIX}"-nginx
+
+  printf "${MAIN}The Docker images have been removed.${DEFAULT}\n"
+else
+  printf "${SUB}The Docker images were preserved.${DEFAULT}\n"
+fi
+
+read -p "Remove corresponding Redis and MySQL volumes? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  printf "${MAIN}Removing volume: ${SUB}%s${DEFAULT}..\n" "${CONTAINER_PREFIX}"-mysql
+  docker volume rm --force "${CONTAINER_PREFIX}"-mysql
+
+  printf "${MAIN}Removing volume: ${SUB}%s${DEFAULT}..\n" "${CONTAINER_PREFIX}"-redis
+  docker volume rm --force "${CONTAINER_PREFIX}"-redis
+
+  printf "${MAIN}The corresponding volumes have been removed.${DEFAULT}\n"
+else
+  printf "${SUB}The corrseponding volumes were preserved.${DEFAULT}\n"
+fi
+
+echo All done.
+
+exit 0
