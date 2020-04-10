fc3894ed583c83904befbec1ef206ed92ba54f63
Matthijs Lubbers <m.lubbers@gmail.com>
2020-04-10 02:28:52 +0200

    Reformat styling of the commands list

--- develop.sh
+++ develop.sh
@@ -7,68 +7,69 @@ CONTAINER_PREFIX="${COMPOSE_PROJECT_NAME[1]}"
 
 # Set output colors and spacing
 DEFAULT="\033[0m"
-CATEGORY="\033[32m"
-COMMAND="\033[33m"
+CATEGORY="\033[33m"
+COMMAND="\033[32m"
 SPACING="   "
 
 if [ -z "$1" ] || [ "$1" == "help" ] || [ "$1" == "commands" ]; then
-  printf "Available commands:\n\n"
+  printf "${DEFAULT}Use ./develop.sh ${COMMAND}<command>${DEFAULT}\n\n"
 
-  printf "${CATEGORY}Assets\n"
+  printf "${DEFAULT}Available ${COMMAND}commands${DEFAULT} per ${CATEGORY}category${DEFAULT}:\n"
+  printf "${CATEGORY} Assets\n"
   printf "${COMMAND}  run-dev      ${SPACING}${DEFAULT}Compile assets for development\n"
   printf "${COMMAND}  run-prod     ${SPACING}${DEFAULT}Compile assets for production\n"
-  printf "${COMMAND}  watch        ${SPACING}${DEFAULT}Run scripts from package.json when files change\n\n"
+  printf "${COMMAND}  watch        ${SPACING}${DEFAULT}Run scripts from package.json when files change\n"
 
-  printf "${CATEGORY}Composer\n"
+  printf "${CATEGORY} Composer\n"
   printf "${COMMAND}  install      ${SPACING}${DEFAULT}Install dependencies\n"
   printf "${COMMAND}  install-dry  ${SPACING}${DEFAULT}Fake install dependencies\n"
   printf "${COMMAND}  outdated     ${SPACING}${DEFAULT}List outdated dependencies\n"
   printf "${COMMAND}  update       ${SPACING}${DEFAULT}Update dependencies\n"
-  printf "${COMMAND}  update-dry   ${SPACING}${DEFAULT}Fake update dependencies\n\n"
+  printf "${COMMAND}  update-dry   ${SPACING}${DEFAULT}Fake update dependencies\n"
 
-  printf "${CATEGORY}Database\n"
-  printf "${COMMAND}  fresh|refresh${SPACING}${DEFAULT}Drop all tables and re-run all migrations\n"
-  printf "${COMMAND}  fresh-seed   ${SPACING}${DEFAULT}Drop all tables, re-run all migrations and seeders\n"
+  printf "${CATEGORY} Database\n"
+  printf "${COMMAND}  fresh|refresh${SPACING}${DEFAULT}Drop all tables and run all migrations\n"
+  printf "${COMMAND}  fresh-seed   ${SPACING}${DEFAULT}Drop all tables and run all migrations and seeders\n"
   printf "${COMMAND}  migrate      ${SPACING}${DEFAULT}Run the database migrations\n"
-  printf "${COMMAND}  seed         ${SPACING}${DEFAULT}Seed the database with records\n\n"
+  printf "${COMMAND}  seed         ${SPACING}${DEFAULT}Seed the database with records\n"
 
-  printf "${CATEGORY}Docker\n"
+  printf "${CATEGORY} Docker\n"
   printf "${COMMAND}  build|rebuild${SPACING}${DEFAULT}Build the images without cache\n"
   printf "${COMMAND}  down         ${SPACING}${DEFAULT}Stop and remove the containers\n"
   printf "${COMMAND}  ps           ${SPACING}${DEFAULT}List the containers\n"
   printf "${COMMAND}  restart      ${SPACING}${DEFAULT}Restart the containers\n"
   printf "${COMMAND}  restart-down ${SPACING}${DEFAULT}Restart the containers using down\n"
   printf "${COMMAND}  stop         ${SPACING}${DEFAULT}Stop the containers\n"
-  printf "${COMMAND}  up           ${SPACING}${DEFAULT}Start the containers\n\n"
+  printf "${COMMAND}  up           ${SPACING}${DEFAULT}Start the containers\n"
 
-  printf "${CATEGORY}Inspection\n"
+  printf "${CATEGORY} Inspection\n"
   printf "${COMMAND}  coverage     ${SPACING}${DEFAULT}Run PHPunit code coverage analysis with PCOV\n"
   printf "${COMMAND}  cs           ${SPACING}${DEFAULT}Show codestyle issues with PHP-CS-Fixer\n"
   printf "${COMMAND}  fix          ${SPACING}${DEFAULT}Fix codestyle issues with PHP-CS-Fixer\n"
-  printf "${COMMAND}  stan         ${SPACING}${DEFAULT}Run static analysis with larastan\n\n"
+  printf "${COMMAND}  stan         ${SPACING}${DEFAULT}Run static analysis with larastan\n"
 
-  printf "${CATEGORY}Logging\n"
-  printf "${COMMAND}  log|logs     ${SPACING}${DEFAULT}Tail logs, use optional 1st argument to specify a service (mysql,nginx,php,redis)\n\n"
+  printf "${CATEGORY} Logging\n"
+  printf "${COMMAND}  log|logs     ${SPACING}${DEFAULT}Tail logs, use optional 1st argument to specify a service (mysql,nginx,php,redis)\n"
 
-  printf "${CATEGORY}Optimization\n"
+  printf "${CATEGORY} Optimization\n"
   printf "${COMMAND}  cache|clear  ${SPACING}${DEFAULT}Clear all the cache\n"
-  printf "${COMMAND}  ide-helper   ${SPACING}${DEFAULT}Create IDE autocompletion files\n\n"
+  printf "${COMMAND}  ide-helper   ${SPACING}${DEFAULT}Create IDE autocompletion files\n"
 
-  printf "${CATEGORY}Routes\n"
+  printf "${CATEGORY} Routes\n"
   printf "${COMMAND}  routes       ${SPACING}${DEFAULT}List all routes\n"
   printf "${COMMAND}  routes-method${SPACING}${DEFAULT}List routes filtered by method use 1st argument as filter-value\n"
   printf "${COMMAND}  routes-name  ${SPACING}${DEFAULT}List routes filtered by name, use 1st argument as filter-value\n"
-  printf "${COMMAND}  routes-path  ${SPACING}${DEFAULT}List routes filtered by path, use 1st argument as filter-value\n\n"
+  printf "${COMMAND}  routes-path  ${SPACING}${DEFAULT}List routes filtered by path, use 1st argument as filter-value\n"
 
-  printf "${CATEGORY}Testing\n"
+  printf "${CATEGORY} Testing\n"
   printf "${COMMAND}  feature      ${SPACING}${DEFAULT}Run feature tests, use optional 1st argument as filter-value\n"
   printf "${COMMAND}  test|tests   ${SPACING}${DEFAULT}Run all tests, use optional 1st argument as filter-value\n"
-  printf "${COMMAND}  unit         ${SPACING}${DEFAULT}Run unit tests, use optional 1st argument as filter-value\n\n"
+  printf "${COMMAND}  unit         ${SPACING}${DEFAULT}Run unit tests, use optional 1st argument as filter-value\n"
 
-  printf "${CATEGORY}Other\n"
-  printf "${COMMAND}  artisan      ${SPACING}${DEFAULT}List artisan commands\n"
+  printf "${CATEGORY} Other\n"
+  printf "${COMMAND}  art          ${SPACING}${DEFAULT}Run artisan commands on the php container\n"
   printf "${COMMAND}  bash|enter   ${SPACING}${DEFAULT}Run bash in the php container\n"
-  printf "${COMMAND}  *            ${SPACING}${DEFAULT}Will be run in the php container\n"
+  printf "${COMMAND}  *            ${SPACING}${DEFAULT}Anything else will be run in the php container, e.g. \"php -m | grep mysql\"\n"
   exit 0
 fi
 
@@ -216,8 +217,8 @@ case "$1" in
     addCommandForTarget container "$(getTestBaseCommandForLaravelMainVersion)$([[ $# -gt 1 ]] && echo " --filter ${*:2}")" ;;
 
   # Other
-  artisan)
-    addCommandForTarget container "php artisan list" ;;
+  art)
+    addCommandForTarget container "php artisan $([[ $# -gt 1 ]] && echo "${*:2}")" ;;
   bash|enter)
     addCommandForTarget container "bash" ;;
   *)
